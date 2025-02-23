15efaa0abaa08e4ca32a9fc27afc466745b3096e
Hubert Czajka <hubert@data.pl>
2025-02-23 14:35:07 +0100

    arduino pin and packet

---
+++ drivers/arduino/arduino_pin.go
@@ -0,0 +1,70 @@
+package arduino
+
+import "fmt"
+
+type PinState uint8
+
+const (
+	PinStateUnknown PinState = 0
+	PinStateLow     PinState = 1
+	PinStateHigh    PinState = 2
+)
+
+func (ps PinState) String() string {
+	switch ps {
+	case PinStateLow:
+		return "LO"
+	case PinStateHigh:
+		return "HI"
+	default:
+		return "NA"
+	}
+}
+
+type Pin struct {
+	number  uint8
+	isInput bool
+	pullup  bool
+	state   PinState
+}
+
+func NewInputPin(number uint8, pullup bool) Pin {
+	return Pin{
+		number:  number,
+		isInput: true,
+		pullup:  pullup,
+		state:   PinStateUnknown,
+	}
+}
+
+func NewOutputPin(number uint8) Pin {
+	return Pin{
+		number:  number,
+		isInput: false,
+		state:   PinStateUnknown,
+	}
+}
+
+func (pi Pin) ConfigEquals(other Pin) bool {
+	if pi.isInput {
+		return pi.isInput == other.isInput && pi.pullup == other.pullup
+	}
+
+	return pi.isInput == other.isInput
+}
+
+func (pi Pin) String() string {
+	dirString := ""
+	if pi.isInput {
+		dirString = "IN "
+		if pi.pullup {
+			dirString += "PULLUP"
+		} else {
+			dirString += "______"
+		}
+	} else {
+		dirString = "OUT" + "______"
+	}
+
+	return fmt.Sprintf("pin [no: %2d] [%s] %s", pi.number, dirString, pi.state.String())
+}
---
+++ drivers/arduino/packet.go
@@ -0,0 +1,109 @@
+package arduino
+
+import (
+	"errors"
+	"hash/crc32"
+)
+
+// Packet overhead with CRC32 checksum, packet length and tag
+const PacketOverhead uint = 4 + 2 + 1
+
+type PacketType byte
+
+const (
+	PACKET_TYPE_NONE                PacketType = 0x00
+	PACKET_TYPE_ARDUINOPRO_CONFIG              = 'C'
+	PACKET_TYPE_ARDUINOPRO_STATUS              = 'S'
+	PACKET_TYPE_ARDUINOPRO_COMMAND             = 'D'
+	PACKET_TYPE_ARDUINOPRO_RESPONSE            = 'R'
+	PACKET_TYPE_ARDUINOPRO_NOTREADY            = 'N'
+)
+
+func (pt PacketType) String() string {
+	switch pt {
+	case PACKET_TYPE_NONE:
+		return "PACKET_TYPE_NONE"
+	case PACKET_TYPE_ARDUINOPRO_CONFIG:
+		return "PACKET_TYPE_ARDUINOPRO_CONFIG"
+	case PACKET_TYPE_ARDUINOPRO_STATUS:
+		return "PACKET_TYPE_ARDUINOPRO_STATUS"
+	case PACKET_TYPE_ARDUINOPRO_COMMAND:
+		return "PACKET_TYPE_ARDUINOPRO_COMMAND"
+	default:
+		return "PACKET_TYPE_UNKNOWN"
+	}
+}
+
+// Packet represents a data packet (received or to be send over UDP)
+// data packet construction:
+// * packet size (2 bytes)
+// * packet tag (1 byte)
+// * actual data/command (variable size)
+// * crc32 checksum (4 bytes)
+type Packet struct {
+	tag  PacketType
+	data []byte
+}
+
+func (pck Packet) PacketType() PacketType {
+	return pck.tag
+}
+
+func (pck Packet) Data() []byte {
+	return pck.data
+}
+
+func (pck Packet) Len() int {
+	return len(pck.data) + int(PacketOverhead)
+}
+
+func (pck Packet) GetRawData() []byte {
+	rawBytes := []byte{}
+	packetLen := uint16(pck.Len())
+	// Convert packet length to byte
+	rawBytes = append(rawBytes, byte(packetLen>>8)) // Most significant byte
+	rawBytes = append(rawBytes, byte(packetLen))    // Least significant byte
+	rawBytes = append(rawBytes, byte(pck.tag))
+	rawBytes = append(rawBytes, pck.data...)
+
+	crcChecksum := crc32.ChecksumIEEE(rawBytes)
+
+	rawBytes = append(rawBytes, byte(crcChecksum>>24)) // Most significant byte
+	rawBytes = append(rawBytes, byte(crcChecksum>>16))
+	rawBytes = append(rawBytes, byte(crcChecksum>>8))
+	rawBytes = append(rawBytes, byte(crcChecksum)) // Least significant byte
+
+	return rawBytes
+}
+
+func ParsePacket(rawData []byte) (Packet, error) {
+	if len(rawData) < 4 {
+		return Packet{}, errors.New("packet size too small")
+	}
+
+	var packetLength uint16
+	packetLength = uint16(rawData[0])<<8 | uint16(rawData[1])
+
+	if len(rawData) != int(packetLength) {
+		return Packet{}, errors.New("packet length mismatch")
+	}
+
+	var packetTag byte
+	packetTag = rawData[2]
+
+	crcBytes := rawData[len(rawData)-4:]
+	crcChecksum := uint32(crcBytes[0])<<24 | uint32(crcBytes[1])<<16 | uint32(crcBytes[2])<<8 | uint32(crcBytes[3])
+
+	if crcChecksum != crc32.ChecksumIEEE(rawData[:len(rawData)-4]) {
+		return Packet{}, errors.New("crc checksum not matched")
+	}
+
+	return Packet{
+		tag:  PacketType(packetTag),
+		data: rawData[3 : len(rawData)-4],
+	}, nil
+}
+
+func NewPacket(packetType PacketType, data []byte) Packet {
+	return Packet{tag: packetType, data: data}
+}
